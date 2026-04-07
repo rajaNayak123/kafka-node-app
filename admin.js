c8152dc4f0b761ea83e7adbe62e1fd3e77f5d3d8
rajaNayak123 <nayakraja151@gmail.com>
2026-04-07 13:01:25 +0530

    add Kafka admin setup to create topic

---
+++ admin.js
@@ -0,0 +1,24 @@
+import { kafka } from "./kafka-client.js";
+
+async function init(){
+    const admin = kafka.admin();
+    console.log("Admin is connecting...");
+
+    await admin.connect();
+    console.log("Admin connect success...");
+    
+
+    console.log("The test-topic is creating...");
+    await admin.createTopics({
+        topics:[{    
+            topic: 'test-topic',
+            numPartitions: 2,
+            replicationFactor: 1,
+        }]
+    })
+    console.log('Topics created successfully');
+
+    await admin.disconnect();
+}
+
+init();
